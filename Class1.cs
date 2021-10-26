7565d8cb6a55c0a0d4d94dbbd3abcacd896770ad
JoJo <joseph.salomone@dealertrack.com>
2021-10-25 17:30:17 -0700

    Fix Anglerfish

--- Class1.cs
+++ Class1.cs
@@ -1,6 +1,7 @@
 ﻿using OWML.Common;
 using OWML.ModHelper;
 using OWML.ModHelper.Events;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,171 +11,128 @@ namespace ClassLibrary2
     public class MainClass : ModBehaviour
     {
         bool isEnabled = true;
-        bool disableShip = false;
-        float loopLength = 1320f;
         float damageMultiplier = 1f;
-        float maximumFuel = 0f;
-        float maximumOxygen = 10f;
-        float anglerSpeedMultiplier = 1f;
-        float anglerDetectMultiplier = 1f;
-        float _lastPlayerHealth;
-        float _lastSuitHealth;
+        float _lastPlayerHealth = 100f;
+        int _lastSuitHealth = 0;
 
         void Start()
         {
             if (isEnabled)
             {
-                ModHelper.Events.Player.OnPlayerAwake += (body) => onAwake();
-                ModHelper.Console.WriteLine("Hard Mode Enabled!");
+                ModHelper.Events.Player.OnPlayerAwake += (player) => onAwake();
+                Anglerfish.Start();
+                ModHelper.Console.WriteLine("Hard Mode: ready!");
             }
+        }
+
+        void Destory()
+        {
+            Anglerfish.Destory();
+            ModHelper.Console.WriteLine("Hard Mode: clean up!");
+        }
+
+        private T getConfigOrDefault<T>(IModConfig config, string id, T defaultValue)
+        {
+            try
+            {
+                var sValue = config.GetSettingsValue<T>(id);
+                if (sValue == null)
+                {
+                    throw new NullReferenceException(id);
+                }
+                if (sValue is string && ((string)(object)sValue).Length < 1)
+                {
+                    throw new NullReferenceException(id);
+                }
+                return sValue;
+            }
+            catch (Exception e)
+            {
+                config.SetSettingsValue(id, defaultValue);
+                return defaultValue;
+            };
         }
 
         public override void Configure(IModConfig config)
         {
             isEnabled = config.Enabled;
 
-            loopLength = config.GetSettingsValue<float>("Loop Duration");
-            damageMultiplier = config.GetSettingsValue<float>("Damage Multiplier");
-            maximumFuel = config.GetSettingsValue<float>("Fuel Percentage");
-            maximumOxygen = config.GetSettingsValue<float>("Oxygen Percentage");
-            disableShip = config.GetSettingsValue<bool>("Disable Ship");
-            anglerSpeedMultiplier = config.GetSettingsValue<float>("Anglerfish Speed Multiplier");
-            anglerDetectMultiplier = config.GetSettingsValue<float>("Anglerfish Detection Multiplier");
+            SuperNova.maxDuration = getConfigOrDefault<float>(config, "Loop Duration", 1320f);
+            damageMultiplier = getConfigOrDefault<float>(config, "Damage Multiplier", 1f);
+            Player.maxFuelSeconds = getConfigOrDefault<float>(config, "Fuel Percentage", 100f);
+            Ship.maxFuelSeconds = Player.maxFuelSeconds * 100f;
+            Player.maxOxygenSeconds = getConfigOrDefault<float>(config, "Oxygen Percentage", 100f) * 45f;
+            Ship.maxOxygenSeconds = Player.maxOxygenSeconds * (600f / 45f);
+            Ship.maxFuelSeconds = getConfigOrDefault<bool>(config, "Disable Ship", false) ? 0f : Ship.maxFuelSeconds;
+            var anglerSpeedMultiplier = getConfigOrDefault<float>(config, "Anglerfish Speed Multiplier", 1f);
+            var anglerDetectMultiplier = getConfigOrDefault<float>(config, "Anglerfish Detection Multiplier", 1f);
 
             updateAnglerFish(anglerSpeedMultiplier, anglerDetectMultiplier);
 
-            ModHelper.Console.WriteLine("Hard Mode Configured!");
+            ModHelper.Console.WriteLine("Hard Mode: Configured!");
         }
 
         void OnGUI()
         {
         }
 
-        private void onAwake()
+        void onAwake()
         {
-            updateAnglerFish(anglerSpeedMultiplier, anglerDetectMultiplier);
+            Anglerfish.Awake();
+
+            ModHelper.Console.WriteLine("Hard Mode: Player Awakes");
         }
 
         void Update()
         {
-            updateLoopDuration(loopLength);
-            updateHealth(damageMultiplier);
-            setMaximumFuel(maximumFuel);
-            setMaximumOxygen(maximumOxygen);
-            if (disableShip && Locator.GetShipTransform() && Locator.GetShipTransform().GetComponent<ShipResources>())
+            if (isEnabled)
             {
-                Locator.GetShipTransform().GetComponent<ShipResources>().SetFuel(0f);
-            }
-        }
+                Player.Update();
+                Ship.Update();
+                Anglerfish.Update();
+                SuperNova.Update();
 
-        private void updateLoopDuration(float duration)
-        {
-            if (TimeLoop.GetSecondsRemaining() > duration)
-            {
-                TimeLoop.SetSecondsRemaining(duration);
+                updateHealth(damageMultiplier);
             }
         }
 
         private void updateAnglerFish(float speedMultiplier, float detectMultiplier)
         {
-            foreach (AnglerfishController anglerfishController in UnityEngine.Object.FindObjectsOfType<AnglerfishController>())
-            {
-                anglerfishController.SetValue("_arrivalDistance", 100f * detectMultiplier);
-                anglerfishController.SetValue("_pursueDistance", 200f * detectMultiplier);
-                anglerfishController.SetValue("_escapeDistance", 500f * detectMultiplier);
+            Anglerfish.mouthOpenDistance = 100f * detectMultiplier;
+            Anglerfish.pursueDistance = 200f * detectMultiplier;
+            Anglerfish.escapeDistance = 500f * detectMultiplier;
 
-                anglerfishController.SetValue("_acceleration", 2f * speedMultiplier);
-                anglerfishController.SetValue("_investigateSpeed", 20f * speedMultiplier);
-                anglerfishController.SetValue("_chaseSpeed", 42f * speedMultiplier);
-                anglerfishController.SetValue("_turnSpeed", 100f * speedMultiplier);
-                anglerfishController.SetValue("_quickTurnSpeed", 360f * speedMultiplier);
-            }
+            Anglerfish.acceleration = 2f * speedMultiplier;
+            Anglerfish.investigateSpeed = 20f * speedMultiplier;
+            Anglerfish.chaseSpeed = 42f * speedMultiplier;
+            Anglerfish.turnSpeed = 100f * speedMultiplier;
+            Anglerfish.quickTurnSpeed = 360f * speedMultiplier;
         }
 
         private void updateHealth(float damageMultiplier)
         {
-            if (Locator.GetPlayerTransform() && Locator.GetPlayerTransform().GetComponent<PlayerResources>())
+            var damage = _lastPlayerHealth - Player.health;
+            if (damage > 0)
             {
-                var resources = Locator.GetPlayerTransform().GetComponent<PlayerResources>();
-                var damage = _lastPlayerHealth - resources.GetHealth();
-                if (damage > 0)
-                {
-                    _lastPlayerHealth = _lastPlayerHealth - (damageMultiplier * damage);
-                    resources.SetValue("_currentHealth", _lastPlayerHealth);
-                }
-                else
-                {
-                    _lastPlayerHealth = Locator.GetPlayerTransform().GetComponent<PlayerResources>().GetHealth();
-                }
+                _lastPlayerHealth = _lastPlayerHealth - (damageMultiplier * damage);
+                Player.health = _lastPlayerHealth;
+            }
+            else if (damage < 0)
+            {
+                _lastPlayerHealth = Player.health;
+            }
 
-                damage = resources.GetValue<int>("_currentNumPunctures") - _lastSuitHealth;
-                if (damage > 0)
-                {
-                    _lastSuitHealth = _lastSuitHealth + (damageMultiplier * damage);
-                    resources.SetValue("_lastSuitHealth", (int)_lastSuitHealth);
-                }
-                else
-                {
-                    _lastSuitHealth = resources.GetValue<int>("_currentNumPunctures");
-                }
-            }
-            if (Locator.GetShipTransform() && Locator.GetShipTransform().GetComponent<ShipDamageController>())
+            var punctures = Player.suitPunctureCount - _lastSuitHealth;
+            if (punctures > 0)
             {
-                var resources = Locator.GetShipTransform().GetComponent<ShipResources>();
-                // TODO
+                _lastSuitHealth = _lastSuitHealth + (int)(damageMultiplier * punctures);
+                Player.suitPunctureCount = _lastSuitHealth;
             }
-        }
-
-        private void setMaximumFuel(float maximumFuel)
-        {
-            if (Locator.GetPlayerTransform() && Locator.GetPlayerTransform().GetComponent<PlayerResources>())
+            else if (punctures < 0)
             {
-                var resources = Locator.GetPlayerTransform().GetComponent<PlayerResources>();
-                var playerFuel = resources.GetFuel();
-                if (playerFuel > maximumFuel)
-                {
-                    if (Locator.GetPlayerTransform().GetComponent<PlayerResources>().IsRefueling())
-                    {
-                        Locator.GetPlayerTransform().GetComponent<PlayerResources>().StopRefillResources();
-                    }
-                    resources.SetValue("_currentFuel", maximumFuel);
-                }
+                _lastSuitHealth = Player.suitPunctureCount;
             }
-            if (Locator.GetShipTransform() && Locator.GetShipTransform().GetComponent<ShipResources>())
-            {
-                var resources = Locator.GetShipTransform().GetComponent<ShipResources>();
-                var shipFuel = resources.GetFuel();
-                if (shipFuel > maximumFuel * 100f)
-                {
-                    resources.SetFuel(maximumFuel * 100f);
-                }
-            }
-        }
-
-        private void setMaximumOxygen(float maximumOxygen)
-        {
-            if (Locator.GetPlayerTransform() && Locator.GetPlayerTransform().GetComponent<PlayerResources>())
-            {
-                var resources = Locator.GetPlayerTransform().GetComponent<PlayerResources>();
-                var playerOxygen = resources.GetValue<float>("_currentOxygen");
-                if (!resources.IsOxygenPresent() && playerOxygen > (maximumOxygen * 4.5f))
-                {
-                    if (resources.IsRefillingOxygen())
-                    {
-                        resources.SetValue("_refillingOxygen", false);
-                    }
-                    resources.SetValue("_currentOxygen", maximumOxygen * 4.5f);
-                }
-            }
-            if (Locator.GetShipTransform() && Locator.GetShipTransform().GetComponent<ShipResources>())
-            {
-                var resources = Locator.GetShipTransform().GetComponent<ShipResources>();
-                var shipOxygen = resources.GetOxygen();
-                if (shipOxygen > maximumOxygen * 60f)
-                {
-                    resources.SetOxygen(maximumOxygen * 60f);
-                }
-            }
+            // TODO ship damagae multiplier
         }
     }
 }
---
+++ SuperNova.cs
@@ -0,0 +1,68 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+using OWML.ModHelper.Events;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+namespace ClassLibrary2
+{
+    static class SuperNova
+    {
+        public static float maxDuration { get; set; } = 1360f;
+        private static float supernovaTimer = -1f;
+        public static void Reset()
+        {
+            supernovaTimer = -1f;
+        }
+
+        public static void Update()
+        {
+            if (supernovaTimer >= 0f)
+            {
+                TimeLoop.SetSecondsRemaining(supernovaTimer);
+            }
+            if (TimeLoop.GetSecondsRemaining() > maxDuration)
+            {
+                set(maxDuration);
+            }
+        }
+
+        public static void set(float seconds)
+        {
+            TimeLoop.SetSecondsRemaining(seconds);
+            if (supernovaTimer >= 0f)
+            {
+                supernovaTimer = TimeLoop.GetSecondsRemaining();
+            }
+        }
+
+        public static void adjust(float amount)
+        {
+            TimeLoop.SetSecondsRemaining(TimeLoop.GetSecondsRemaining() + amount);
+            if (supernovaTimer >= 0f)
+            {
+                supernovaTimer = TimeLoop.GetSecondsRemaining();
+            }
+        }
+
+        public static void freeze(bool stopTime)
+        {
+            if (stopTime)
+            {
+                supernovaTimer = TimeLoop.GetSecondsRemaining();
+            }
+            else
+            {
+                supernovaTimer = -1f;
+            }
+        }
+
+        public static bool isFrozen()
+        {
+            return supernovaTimer >= 0f;
+        }
+    }
+}
